b945a1291690cce055416a263586d91367042adb
alex-palmer <oleg.ketrar.accounts@icloud.com>
2015-09-17 12:09:45 +0300

    start project

---
+++ Test_Marvel_API/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test_Marvel_API
+//
+//  Created by Oleg Ketrar on 17.09.15.
+//  Copyright © 2015 Oleg Ketrar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test_Marvel_API/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Test_Marvel_API
+//
+//  Created by Oleg Ketrar on 17.09.15.
+//  Copyright © 2015 Oleg Ketrar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
